7b9e5edc71ff84c5b4367af0b698747a8379ae0c
Joshua Uwuseba <joshuwus@gmail.com>
2019-09-17 14:53:49 +0100

    This sianother commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #Heroku: Update database configuration for $DATABASE_URL
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['DEFAULT'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
